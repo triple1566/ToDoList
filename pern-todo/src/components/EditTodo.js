d1834ba0b454637c1baee9d206b4ab6e514c69d4
triple1566 <ljhub1566@gmail.com>
2024-08-19 17:57:16 -0400

    bugfix: modal edit content not updating fixed

--- pern-todo/src/components/EditTodo.js
+++ pern-todo/src/components/EditTodo.js
@@ -28,20 +28,20 @@ const EditTodo = ({todo}) => {
             Edit
         </button>
 
-        <div className="modal" id={`id${todo.todo_id}`}>
+        <div className="modal" id={`id${todo.todo_id}`} onClick={()=>setDescription(todo.description)}>
             <div className="modal-dialog">
                 <div className="modal-content">
 
                     <div className="modal-header">
                         <h4 className="modal-title">Edit Todo</h4>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={()=>{setDescription(todo.description)}}></button>
                     </div>
                     <div className="modal-body">
                         <input type='text' className='form-control' value={description} onChange={e=>setDescription(e.target.value)}/>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-warning" data-bs-dismiss="modal" onClick={e=>updateDescription(e)}>Save Changes</button>
-                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={()=>setDescription(todo.description)}>Close</button>
                     </div>
 
                 </div>
