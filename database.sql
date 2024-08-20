f7ae09a34ebaa42267476d7646d02b02d5336aeb
triple1566 <ljhub1566@gmail.com>
2024-08-19 20:53:52 -0400

    database.sql ready for heroku deploy

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE perntodo;
-
+--CREATE DATABASE perntodo;
+--commented out for pipe use in heroku postgres terminal
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
