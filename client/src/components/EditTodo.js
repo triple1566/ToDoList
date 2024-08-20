b2169ad994747515000ffb0122e0ebf4ef83f0cb
triple1566 <ljhub1566@gmail.com>
2024-08-19 20:29:47 -0400

    replace absolute http request with proxy

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -9,7 +9,11 @@ const EditTodo = ({todo}) => {
         e.preventDefault();
         try {
             const body = {description};
-            const response = await fetch(`http://localhost:5001/todos/${todo.todo_id}`, {
+
+            //proxy
+
+
+            const response = await fetch(`/todos/${todo.todo_id}`, {
                 method: 'PUT',
                 headers: {'Content-Type':'application/json'},
                 body: JSON.stringify(body)
